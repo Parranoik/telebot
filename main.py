51779964d70232510cb1c37728c1e0c8a2bf508a
Parranoik <jd8887@yandex.ru>
2023-09-06 11:03:09 +0300

    using functions for every 1 callback typefile = 1 handler func. basic logger is implemented and include in send_echo_text func

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def hello(message):
 
 bot.infinity_polling()
 '''
-
-from aiogram import Bot, Dispatcher
+import json
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -33,33 +33,55 @@ API_TOKEN: str = os.getenv('BOT_TOKEN') #os.getenv('BOT_TOKEN') //нужный �
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
+def save_inf(message: Message): #logging to file
+    with open('log.json', 'a', encoding='utf-8', newline='\n') as log:
+        log.write(message.model_dump_json(indent=4, exclude_none=True))
 
-# Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
-    print(message) #отладка
-    print(message.date)  #отладка
-    print(message.chat.username) #отладка
 
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение')
-    print(message.text) #отладка
-    print(message.date) #отладка
-    print(message.chat.username) #отладка
+    await message.answer('Напиши мне что-нибудь и в ответ я пришлю тебе твое сообщение')
 
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
-async def send_echo(message: Message):
+async def send_echo_text(message: Message):
     await message.reply(text=message.text)
-    print(message) #отладка
-    print(message.date) #отладка
-    print(message.chat.username) #отладка
+    save_inf(message)
+
+async def send_echo_photo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+async def send_echo_video(message: Message):
+    await message.reply_video(message.video.file_id)
+
+
+async def send_echo_sticker(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
+
+async def send_echo_audio(message: Message):
+    await message.reply_audio(message.audio.file_id)
+
+async def send_echo_voice(message: Message):
+    await message.reply_voice(message.voice.file_id)
+
+async def send_echo_document(message: Message):
+    await message.reply_document(message.document.file_id)
+
+
+
+
+
+
+dp.message.register(process_start_command, Command(commands=['start', 'strat', 'strt', 'satrt'], ignore_case=True))
+dp.message.register(process_help_command, Command(commands=['info', 'inf', 'ifno', 'help', 'hlp', 'hlep'], ignore_case=True))
+dp.message.register(send_echo_photo, F.photo)
+dp.message.register(send_echo_video, F.video)
+dp.message.register(send_echo_audio, F.audio)
+dp.message.register(send_echo_voice, F.voice)
+dp.message.register(send_echo_sticker, F.sticker)
+dp.message.register(send_echo_document, F.document)
+dp.message.register(send_echo_text, F.text)
+def fff(message: Message):
+    print(message.model_dump_json(indent=4, exclude_none=True))
 
 if __name__ == '__main__':
     dp.run_polling(bot)
